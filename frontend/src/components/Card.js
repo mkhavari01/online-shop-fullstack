9af8d895d8eb5a38907d39e0323261b793199080
Mahdi Khavari <mkhavari0120@gmail.com>
2022-12-10 22:12:44 +0330

    add card styles for homepage

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,11 +1,25 @@
-const Card = ({ post }) => {
+import "../assets/css/card.css";
+
+const Card = ({ item }) => {
   return (
-    <div className="card">
-      <p className="card__title">{post.name}</p>
-      <p className="card__author">{post.price}</p>
-      <p className="card__body">{post.description}</p>
+    <div className="card dir d-flex justify-content-center align-items-center flex-column m-4 p-4">
+      <img
+        width={200}
+        src={process.env.REACT_APP_BACKEND_URL + "/" + item.productImage}
+      />
+      {/* <Avatar
+        alt="N/A"
+        src={`${process.env.REACT_APP_BACKEND_URL + "/" + item.productImage}`}
+        // sx={{ width: 56, height: 56 }}
+      /> */}
+      <h3 className="mt-4">{item.name}</h3>
+      <p>{item.description}</p>
+      <div className="d-flex flex-row-reverse justify-content-around w-100 flex-wrap">
+        <span className="p-3">{item.price} تومان</span>
+        <span className="p-3">{item.grouping}</span>
+      </div>
     </div>
   );
 };
 
-export default Card;
+export { Card };
--- frontend/src/pages/website/Home.js
+++ frontend/src/pages/website/Home.js
@@ -1,12 +1,36 @@
+import { Card } from "components/Card";
 import { Layout } from "layout/Layout";
 import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-const Home = (props) => {
-  useEffect(() => {}, []);
+const x = [
+  {
+    name: "name",
+    description: "desc",
+    price: "120,000",
+    grouping: "عطر و خوشبویی",
+    productImage: "uploads//1669242836155ME.jpg",
+  },
+];
+const Home = () => {
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state.products);
+
+  useEffect(() => {
+    dispatch();
+  }, [dispatch]);
+
   return (
     <>
       <Layout />
-      <h1>Home page</h1>
+      <section className="dir m-5">
+        <h1>کالاهای گروه لبنیات</h1>
+        <div className="d-flex flex-wrap">
+          {x.map((el) => {
+            return <Card item={el} />;
+          })}
+        </div>
+      </section>
     </>
   );
 };
