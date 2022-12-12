3edd9349532377e061d649a5a3cfa3ddc38bf857
Mahdi Khavari <mkhavari0120@gmail.com>
2022-12-13 01:35:11 +0330

    add post order to frontend

--- frontend/src/pages/payment/Success.js
+++ frontend/src/pages/payment/Success.js
@@ -1,9 +1,26 @@
+import { ordersApi } from "api/orders.api";
 import { useEffect } from "react";
-
+import { toast } from "react-toastify";
 const Success = () => {
   useEffect(() => {
-    console.log("in here we call post action to set data in backend");
+    let data = JSON.parse(localStorage.getItem("cartInfo"));
+    if (data) {
+      data["products"] = JSON.parse(localStorage.getItem("cart"));
+      console.log("in here we call post action to set data in backend", data);
+      ordersApi
+        .post(data)
+        .then((res) => {
+          localStorage.removeItem("cart");
+          localStorage.removeItem("cartInfo");
+          toast.success("سفارش شما با موفقیت ثبت شد");
+        })
+        .catch((error) => {
+          toast.error("متاسفانه به مشکلی برخوردیم!");
+          console.log("error", error);
+        });
+    }
   }, []);
+
   return (
     <>
       <h1>Success page</h1>
