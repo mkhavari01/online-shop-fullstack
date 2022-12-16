4492db0a4060d2241cf8dca9ac4f31e870d2c089
Mahdi Khavari <mkhavari0120@gmail.com>
2022-12-17 01:17:19 +0330

    add payment result page ui

--- frontend/src/pages/payment/Error.js
+++ frontend/src/pages/payment/Error.js
@@ -1,7 +1,17 @@
+import ErrorIMG from "assets/img/error-img.jpg";
+import { Layout } from "layout/Layout";
+
 const Error = () => {
   return (
     <>
-      <h1>Error page</h1>
+      <Layout></Layout>
+      <div className="d-flex flex-column justify-content-center align-items-center mt-5 text-center">
+        <h1>نتیجه پرداخت</h1>
+        <img src={ErrorIMG} />
+        <p className="mx-5">
+          پرداخت موفقیت آمیز نبود , سفارش شما در انتظار پرداخت میباشد{" "}
+        </p>
+      </div>
     </>
   );
 };
--- frontend/src/pages/payment/Success.js
+++ frontend/src/pages/payment/Success.js
@@ -2,30 +2,38 @@ import { ordersApi } from "api/orders.api";
 import { Layout } from "layout/Layout";
 import { useEffect } from "react";
 import { toast } from "react-toastify";
+import SuccessIMG from "assets/img/success-img.jpg";
 const Success = () => {
-  useEffect(() => {
-    let data = JSON.parse(localStorage.getItem("cartInfo"));
-    if (data) {
-      data["products"] = JSON.parse(localStorage.getItem("cart"));
-      console.log("in here we call post action to set data in backend", data);
-      ordersApi
-        .post(data)
-        .then((res) => {
-          localStorage.removeItem("cart");
-          localStorage.removeItem("cartInfo");
-          toast.success("سفارش شما با موفقیت ثبت شد");
-        })
-        .catch((error) => {
-          toast.error("متاسفانه به مشکلی برخوردیم!");
-          console.log("error", error);
-        });
-    }
-  }, []);
+  // useEffect(() => {
+  //   let data = JSON.parse(localStorage.getItem("cartInfo"));
+  //   if (data) {
+  //     data["products"] = JSON.parse(localStorage.getItem("cart"));
+  //     console.log("in here we call post action to set data in backend", data);
+  //     ordersApi
+  //       .post(data)
+  //       .then((res) => {
+  //         localStorage.removeItem("cart");
+  //         localStorage.removeItem("cartInfo");
+  //         toast.success("سفارش شما با موفقیت ثبت شد");
+  //       })
+  //       .catch((error) => {
+  //         toast.error("متاسفانه به مشکلی برخوردیم!");
+  //         console.log("error", error);
+  //       });
+  //   }
+  // }, []);
 
   return (
     <>
       <Layout />
-      <h1>Success page</h1>
+      <div className="d-flex flex-column justify-content-center align-items-center mt-5 text-center">
+        <h1>نتیجه پرداخت</h1>
+        <img src={SuccessIMG} />
+        <p className="mx-5">
+          با تشکر از پرداخت شما , سفارش شما ثبت شده و جهت هماهنگی ارسال با شما
+          تماس گرفته خواهد شد{" "}
+        </p>
+      </div>
     </>
   );
 };
